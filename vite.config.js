c47953cc2aad7e3b3b7bfa2e9e93b2855c09917c
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-07-08 14:59:16 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import jsconfigPaths from 'vite-jsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/projects-tracker',
+  plugins: [react(), jsconfigPaths()],
+});
